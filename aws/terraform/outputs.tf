0762d80594f2b1f8679b86e7008a545289bcc198
indumathi-ankercloud <indumathi.jayaraman@ankercloud.com>
2025-11-23 20:47:57 +0530

    rm

--- aws/terraform/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "aurora_endpoint" { value = aws_rds_cluster.aurora.endpoint }
-output "dms_replication_instance_arn" { value = aws_dms_replication_instance.rep.replication_instance_arn }
-output "dms_task_id" { value = aws_dms_replication_task.task.replication_task_id }
--- gcp/terraform/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "gcp_project" { type = string }
-variable "gcp_region" { default = "asia-south1" }
-variable "gcp_vpc_name" { default = "mig-gcp-vpc" }
-variable "gcp_vpc_cidr" { default = "10.50.0.0/16" }
-variable "gcp_subnet_cidr" { default = "10.50.3.0/24" }
-variable "aws_vpn_public_ip" { type = string } # AWS VPN public ip from terraform output or console
-variable "vpn_pre_shared_key" { type = string }
